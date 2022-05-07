d9cd470099465c3cffd2437b3f5c65a1cb5b5d54
Egor <ovchinnikovegords@gmail.com>
2022-05-07 17:47:49 +0300

    Add comments and documentation

--- main.py
+++ main.py
@@ -2,23 +2,32 @@ from pathlib import Path
 from gtts import gTTS
 import pdfplumber
 
+# File path
 file: str = r'files\text.pdf'
 
 
 def pdf_to_mp3(file_path=file, language='ru'):
+    """
+    Create .mp3 file and .txt from pdf file
+    :param file_path: path to pdf file
+    :param language: choosen language of file
+    """
 
     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
         with pdfplumber.PDF(open(file_path, mode='rb')) as pdf:
             pages = [page.extract_text() for page in pdf.pages]
 
+        # if we have more than two pages we join all of them
         text = ''.join(pages)
+        # delete all new lines
         text = text.replace('\n', '')
 
+        # write from pdf to txt
         with open(r'results\text_file.txt', 'w') as txt_file:
             txt_file.write(text)
 
+        # create audio and save file
         my_audio = gTTS(text=text, lang=language)
-        # file_name = Path(file_path).stem
         my_audio.save(r'results\result_audio.mp3')
 
         return '[+] result_audio.mp3 successfully saved'
